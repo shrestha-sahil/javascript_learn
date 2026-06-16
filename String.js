fe62eebeeb196664c52d2df6b9fde9441b6bed46
sahil52-git <sahilshrestha741@gmail.com>
2026-06-17 05:36:13 +0545

    String concatenation

---
+++ String.js
@@ -0,0 +1,20 @@
+//String concatenation example
+const name = "sahil"
+const repoCount = 8
+
+// console.log(name + repoCount + " newValue");
+
+console.log(`My name is ${name} and my repoCount is ${repoCount}`);
+
+
+const gameName = new String ('sahil')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase);
+
+
+
