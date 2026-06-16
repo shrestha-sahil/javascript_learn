ad5b6f970a93e1cef64075a6426b810a9630fe3f
sahil52-git <sahilshrestha741@gmail.com>
2026-06-16 16:52:05 +0545

    conversionOperation

---
+++ conversionOperation.js
@@ -0,0 +1,19 @@
+let Score = undefined
+
+let ValueInNumber = Number(Score)
+
+console.log(typeof Score)
+console.log(ValueInNumber)
+
+
+//if undefined then output is NAN
+//if value is null output is object 0
+//if false/true then its boolean
+//if the value is inside this ""  output is string
+
+let isLoggedIn = "sahil"
+
+let isLoggedOut //output undefined 
+
+console.log(isLoggedIn)
+console.log(isLoggedOut)
