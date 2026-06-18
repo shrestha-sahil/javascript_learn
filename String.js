9f0db89d4699fd24930f5da769a98e9fa39f553e
sahil52-git <sahilshrestha741@gmail.com>
2026-06-19 05:02:09 +0545

    playing with string

--- String.js
+++ String.js
@@ -1,5 +1,5 @@
 //String concatenation example
-const name = "sahil"
+const name = "Sahil Shrestha name variable"
 const repoCount = 8
 
 // console.log(name + repoCount + " newValue");
@@ -7,7 +7,7 @@ const repoCount = 8
 console.log(`My name is ${name} and my repoCount is ${repoCount}`);
 
 
-const gameName = new String ('sahil')
+const gameName = new String ('Sahil Shrestha')
 
 console.log(gameName[0]);
 console.log(gameName.__proto__);
@@ -24,6 +24,24 @@ console.log(gameName.indexOf('i'));
 
 const newString = gameName.substring(0,5);  //for accessing the string name value
 console.log(newString);     //method calling
+
+
+const anotherString = gameName.slice(-3, 5) //using of slice method for negative values(-)
+console.log(anotherString);
+
+const newStringOne = "  Sahil   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+console.log(gameName.split('-'));
+
+
+console.log(name.split('-'));
+
+
+
+
 
 
 
