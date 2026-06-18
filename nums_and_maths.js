dc57efbd94d19f4b883032a6528d1ce5537a80c6
sahil52-git <sahilshrestha741@gmail.com>
2026-06-19 05:35:28 +0545

    maths method and its properties played

--- nums_and_maths.js
+++ nums_and_maths.js
@@ -5,5 +5,21 @@ const balance = new Number(99)
 console.log(balance);
 
 console.log(balance.toString().concat);
-console.log(balance.toFixed().length);
+console.log(balance.toFixed(4));
+
+const otherNumber = 193.9386
+
+console.log(otherNumber.toPrecision(3)); //precision method is used for finding out the precise value of a numbers
+
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN ')); //changes the 100000 into 1,00,000 to from english to indian currency
+
+
+//+++++++++++++++++Maths+++++++++++++++++++++++++++
+console.log(Math);
+console.log(Math.abs(1));
+console.log(Math.round(99.99));
+console.log(Math.ceil(999.99));
+
 
