edf26a20bd1e6a6e1a858e31c8a17a67e7a7bcfc
sahil52-git <sahilshrestha741@gmail.com>
2026-06-23 21:12:51 +0545

    learned about json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,5 +1,5 @@
 const tinderUser = new Object()
-console.log(tinderUser);
+// console.log(tinderUser);
 
 
 tinderUser.id = "2004"
@@ -7,26 +7,50 @@ tinderUser.name = "Sahil"
 
 // console.log(tinderUser);
 
-const regularUser = {
-    email : "sahilshrestha741@gmail.com",
-    fullname : {//object
-        userfullname: {
-        firstname : "sahil",//object
-        lastname : "Shrestha"
-        }
-    }
-}
+// const regularUser = {
+//     email : "sahilshrestha741@gmail.com",
+//     fullname : {//object
+//         userfullname: {
+//         firstname : "sahil",//object
+//         lastname : "Shrestha"
+//         }
+//     }
+// }
 //     console.log(regularUser.fullname?.userfullname.firstname);
     
 //     console.log(regularUser.fullname?.userfullname.lastname);
 
     
-    const obj1 = {1: "a", 2: "b"}
-    const obj2 = {3: "c", 4: "d"}
-    const obj4 = {5: "e", 6: "f"}
+    // const obj1 = {1: "a", 2: "b"}
+    // const obj2 = {3: "c", 4: "d"}
+    // const obj4 = {5: "e", 6: "f"}
 
     // const obj3 = {obj1, obj2}
 
     // const obj3 = Object.assign({}, obj1, obj2, obj4) //normall obj assign
-    const obj3 = {...obj1, ...obj2, ...obj4} //using split method instead of above
-    console.log(obj3);
+    // const obj3 = {...obj1, ...obj2, ...obj4} //using split method instead of above
+    // console.log(obj3);
+
+
+   const course = {
+    courseName: "java learn",
+    price: "999",
+    courseLearn: "Sahil"
+   } 
+
+   const {courseLearn} = course
+   
+   console.log(courseLearn);
+   
+
+//json 
+   {
+    "name" : "sahil",
+    "course" : "javascript",
+    "age" : "23"
+   }
+
+   {
+    {},
+    {},
+   }
