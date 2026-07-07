2aa7131b0fb2fe71b4ece37f6ddc41db6044d6fa
sahil52-git <sahilshrestha741@gmail.com>
2026-07-07 20:28:07 +0545

    scopes and function manipulation

---
+++ 03_basics/03_scopes.js
@@ -0,0 +1,49 @@
+// //     let a = 20;
+// //     const b = 22;
+// //     var c = 23;
+
+// // console.log (a);
+// // console.log(b);
+// // console.log(c);
+
+
+// let a = 200;
+// if (true){
+//     // let a = 100;
+//     const b = 20;
+// var c = 30;
+// console.log(b);
+// console.log(c);
+
+
+// // console.log("inside if block", a);
+// }
+// console.log(b);
+// console.log("outside if block", a);
+// console.log(c);
+
+function one(){
+    const username = "sahil"
+
+    function two(){
+        const website = "youtube"
+        console.log(website);
+    }
+    // console.log(website);
+    two()
+}
+
+one()
+
+if (true){
+    const username = "sahil"
+ if (username === "sahil"){
+    const website = "youtube"
+    console.log(username + website);
+    
+console.log("outside if block", username);
+ }    
+ console.log(website);    
+}
+
+// console.log("outside if block", username);
