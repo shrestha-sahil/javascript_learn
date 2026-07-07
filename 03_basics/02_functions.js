c7e1912da4232e2f402d823bc9ce5ceb0fa1ae45
sahil52-git <sahilshrestha741@gmail.com>
2026-07-07 09:11:48 +0545

    function manipulation

--- 03_basics/02_functions.js
+++ 03_basics/02_functions.js
@@ -1,1 +1,22 @@
-console.log("heelo");
+console.log("hey senpai");
+
+for(let i = 0; i<=10; i++){
+    console.log(i);
+}
+
+switch (new Date().getDay()) {
+    case 0:
+        console.log("Today is Sunday");
+        break;
+    case 1:
+        console.log("Today is Monday");
+        break;
+    case 2:
+        console.log("Today is Tuesday");
+        break;
+        default:
+        console.log("Today is not Sunday, Monday or Tuesday");
+}
+
+//switch function 
+
