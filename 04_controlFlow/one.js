9611df67f3f4bae409d220ca4e525389d3678962
sahil52-git <sahilshrestha741@gmail.com>
2026-07-08 20:14:32 +0545

    if-else condition manipulation

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,28 @@
+//if condition
+
+if(3>2){
+    console.log("condition is true");
+}else {
+    console.log("condition is false");
+}
+
+//condition check with variable
+const isUserLoggedin = false;
+
+if(isUserLoggedin){
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
+
+//not equal to operator
+
+if (3 !== 1){
+    console.log("condition is true");
+} 
+
+// < , > , <= , >=, ==, ===(checks also data type), !==(not equal to operator) are the comparison operators
+
+if(isUserLoggedin === false){
+    console.log("yess user is not logged in");
+}
