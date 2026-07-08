588f1f7cef5ec55ffac179c1149aee0a0f2cb673
sahil52-git <sahilshrestha741@gmail.com>
2026-07-08 19:19:04 +0545

    object method practice

--- 03_basics/04_arrow.js
+++ 03_basics/04_arrow.js
@@ -10,6 +10,27 @@
 
 // user.welcomeMessage();
 
+//object method practice
+const userDetails = {
+    firstName : "sahil", 
+    lastName : " shrestha",
+    age : 22,
+    subject : "javascript",
+
+    functionCallUserDetails : function(){
+        console.log(`hey this is the userDetails portal dear ${this.firstName + this.lastName} and your age is : ${this.age}`);
+        console.log(`and your subject is : ${this.subject}`);
+    }
+};
+
+userDetails.functionCallUserDetails();
+
+
+
+
+
+
+
 // function sahil(){
 //     console.log(this);
 // }
@@ -28,6 +49,23 @@
 //     return num1 + num2
 // }
 
+
+// let addTwo = (number1, number2) => {
+//     return number1 + number2
+// }
+
+// console.log(addTwo(10, 30));
+
+
+// let addOne = (numb1, number4) => {
+//     return numb1 + number4;
+// }
+
+
+//     addOne(10, 20)
+// console.log(addOne);
+
+
 // console.log(addTwo(10, 20));
 
 
@@ -37,7 +75,31 @@
 
 // console.log(addTwo(10, 20)); 
 
-function Db (){
-    console.log(`Database connected`)
+// function Db (){
+//     console.log(`Database connected`)
+// }
+// Db()
+
+//Immeditely invoked function expression (IIFE)
+// (function Db(){
+//     console.log(`Database connected`);
+// })();
+
+// ((name) => {
+//     console.log(`Database connected ${name}`);
+// })('sahil')
+
+
+
+//basic arrow function practice 
+const js = (nu1, nu2) => {
+return nu1 + nu2;
 }
-Db()
+console.log(js(500, 500));
+
+// //implicit return 
+// const sahil = (cat, dog) => cat + dog 
+// let string = " hehehehe"
+
+
+// console.log(sahil(100, 300) + string);
