8f10628191b380897d86ef5d2e4c71b6e763e84b
sahil52-git <sahilshrestha741@gmail.com>
2026-07-12 21:06:53 +0545

    in control statement only done upto switch, skipped other statement cause i know😁

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,21 @@
+const month = 4;
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+
+    case 2:
+        console.log("February");
+        break;      
+
+    case 3:
+        console.log("March");
+        break;  
+
+    case 4:
+        console.log("April");
+        break;
+        default:
+        console.log("Invalid month");
+}
